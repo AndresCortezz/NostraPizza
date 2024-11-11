0a366c0f6cfcf36d21d3b7f66d1e6bd411079331
AndresCortezz <andrescortez916@gmail.com>
2024-11-11 15:21:35 -0500

    First commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,17 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+$routes->get('login', 'login::index');
+$routes->get('/', 'Index::index');
+$routes->get('menu', 'Menu::index');
+$routes->get('recepcionPedidos', 'RecepcionPedidos::index');
+$routes->get('preparacionPedidos', 'PreparacionPedidos::index');
+$routes->get('services', 'Servicios::index');
+$routes->get('contact', 'Contacto::index');
+$routes->get('blog', 'Blog::index');
+$routes->get('about', 'About::index');
